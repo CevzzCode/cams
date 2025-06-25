ff93e1572a018aa5e4907c8712bbc6369fb38583
CevzzCode <camillevillaruz807@gmail.com>
2025-06-26 01:21:35 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Aspiring It Student", "Artist", "Animator", "Web Designer"],
+    typeSpeed:100,
+    BackSpeed:100,
+    backDelay:1000,
+    loop: true
+});
